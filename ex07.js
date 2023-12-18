1795a40383cd8cd9b8eec0c4ad645e8ce8b5f262
Wesley Goulart <goulart89@gmail.com>
2023-12-18 15:37:27 -0300

    ex07 - completo

---
+++ ex07.js
@@ -0,0 +1,19 @@
+// Leia dois vetores de 20 posições e calcule um terceiro vetor contendo, nas posições pares os valores do primeiro e nas posições impares os valores do segundo.
+
+const array1 = [0, 2, 4, 6, 8, 10]
+const array2 = [1, 3, 5, 7, 9, 11]
+const size = array1.length + array2.length
+let array3 = []
+let x = 0
+
+
+for (let i = 0; i < size; i++) {
+    if (i % 2 == 0) {
+        array3[i] = array1[x]
+    } else {
+        array3[i] = array2[x]
+        x++
+    }
+}
+
+console.log(array3)
